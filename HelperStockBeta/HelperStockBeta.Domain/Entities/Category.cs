315c9727382b809d122bacd14178034ed0109bc6
victor.icoma@etec.sp.gov.br <victor.icoma@etec.sp.gov.br>
2023-10-15 23:17:09 -0300

    Modelo de estrura anemica

---
+++ HelperStockBeta/HelperStockBeta.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperStockBeta.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+
+        public ICollection<Products> Products { get; set; }
+    }
+}
